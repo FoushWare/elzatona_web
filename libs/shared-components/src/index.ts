4d0a94f0bb3cf19f6580a28befabea7279c94b4e
a.fouad <a.fouadprogrammer@gmail.com>
2025-10-29 23:20:58 +0300

    fix(shared-components): export EnhancedDashboard and EnhancedUserDashboard from package root

--- libs/shared-components/src/index.ts
+++ libs/shared-components/src/index.ts
@@ -6,3 +6,6 @@ export * from './lib/utils';
 export { ErrorBoundary, UserStatistics } from './lib/common';
 // Removed problematic auth components that have import issues
 // export * from './lib';
+// Explicitly export dashboards required by website app
+export { default as EnhancedDashboard } from './lib/common/EnhancedDashboard';
+export { default as EnhancedUserDashboard } from './lib/common/EnhancedUserDashboard';
