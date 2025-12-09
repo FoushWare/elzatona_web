326c37f3047420d6d29fc4b3bb04fe11230f771e
a.fouad <a.fouadprogrammer@gmail.com>
2025-12-09 20:45:04 +0200

    fix: Correct Next.js dir path in tests/config/jest.config.js
    - Changed dir from './' to '../../' to point to project root
    - Fixes Next.js config loading when jest config is in tests/config/

--- tests/config/jest.config.js
+++ tests/config/jest.config.js
@@ -2,7 +2,8 @@ const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files
-  dir: "./",
+  // From tests/config/, go up two levels to project root
+  dir: "../../",
 });
 
 // Add any custom config to be passed to Jest
